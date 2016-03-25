6f8a5d357c8fc4046932007c98584c275bd90d48
riddhiskasliwal <riddhiskasliwal@gmail.com>
2016-03-25 23:53:21 +0800

    Player Movements
    Added a player who can move left and right along with the animations.
    There is a bit of latency that can be dealt with later.

---
+++ Ghost/Assets/Player_Movement.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var moveLeft : KeyCode;
+var moveRight : KeyCode;
+
+var speed : float = 10;
+
+function Update () 
+{
+	if (Input.GetKey(moveRight)) 
+	{
+		GetComponent.<Rigidbody2D>().velocity.x = speed;
+	}
+	else if (Input.GetKey(moveLeft))
+	{
+		GetComponent.<Rigidbody2D>().velocity.x = - speed;
+	}
+	else 
+	{
+		GetComponent.<Rigidbody2D>().velocity.x = 0;
+	}
+	
+	GetComponent.<Rigidbody2D>().velocity.y = 0;
+}
---
+++ Ghost/Assets/Player_Movements.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Player_Movements : MonoBehaviour {
+
+	public float speed = 10f;
+	private Rigidbody2D player;
+	private Animator anim;
+	private float x, y;
+	// Use this for initialization
+	void Start () {
+		player = gameObject.GetComponent<Rigidbody2D> ();
+		anim = gameObject.GetComponent<Animator> ();
+		x = player.transform.localScale.x;
+		y = player.transform.localScale.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetFloat ("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+		if (Input.GetAxis ("Horizontal") < -0.1f) {
+			transform.localScale = new Vector3(-1*x, y, 1);
+		}
+		if (Input.GetAxis ("Horizontal") > 0.1f) {
+			transform.localScale = new Vector3(x, y, 1);
+		}
+	}
+	void FixedUpdate(){
+		float translation = Input.GetAxis("Horizontal") * speed;
+		translation *= Time.deltaTime;
+		transform.Translate (translation, 0, 0);
+	}
+}
