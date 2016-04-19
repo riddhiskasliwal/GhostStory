c881e7edb74dfb71f3c5e57d246ad490700227af
Angad <angad2102@gmail.com>
2016-04-19 17:00:09 +0800

    Angads Game

---
+++ Ghost/Assets/Angad/water.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class water : MonoBehaviour {
+
+
+	 float height;
+
+
+	// Use this for initialization
+	void Start () {
+		height = -10.09f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(transform.position.y==0)
+		{	print ("Game Over"); 
+		}
+		if (rotate.a == 29) {
+			//print ("won");
+			rotate.block = 0;
+			if (transform.position.y > -10) {
+				height -= 0.005f;
+				transform.position = new Vector3( 0.18f, height, 0.0f );
+			}
+		}
+		if (rotate.a != 29) {
+			height += 0.001f;
+
+			transform.position = new Vector3( 0.18f, height, 0.0f );
+		}
+	}
+}
